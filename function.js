caa588801f3977783cac7ac11dd1542838b1daf5
Aleksandr Feshchuk <aleksfeschuk@gmail.com>
2025-04-05 22:46:35 -0600

    create object calc

--- function.js
+++ function.js
@@ -5,3 +5,10 @@ export function capitalize(str) {
 export function reverseString(str) {
     return str.split('').reverse().join('');
 }
+
+export const calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    divide: (a, b) => a / b,
+    multiply: (a, b) => a * b,
+};
--- functions.test.js
+++ functions.test.js
@@ -7,3 +7,12 @@ import { reverseString } from "./function.js";
 test('reverses a string', () => {
     expect(reverseString('hello').toBe('olleh'));
 });
+
+import { calculator } from "./function.js";
+test('calculator operations', () => {
+    expect(calculator.add(2, 3)).toBe(5);
+    expect(calculator.subtract(5, 2)).toBe(3);
+    expect(calculator.divide(6, 2)).toBe(3);
+    expect(calculator.multiply(2, 4)).toBe(8);
+});
+
