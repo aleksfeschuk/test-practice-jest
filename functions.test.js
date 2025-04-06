0d94343d5f1608824a94faad0cacc004338fb4a1
Aleksandr Feshchuk <aleksfeschuk@gmail.com>
2025-04-05 22:41:31 -0600

    create function reverseString

--- functions.test.js
+++ functions.test.js
@@ -2,3 +2,8 @@ import { capitalize } from "./function.js";
 test('capitalizes the first letter', () => {
     expect(capitalize('hello').toBe('Hello'));
 });
+
+import { reverseString } from "./function.js";
+test('reverses a string', () => {
+    expect(reverseString('hello').toBe('olleh'));
+});
